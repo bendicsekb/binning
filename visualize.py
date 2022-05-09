8debd3d31b5e7d24cd966a2bd50316d73910fa1e
Bendicsek M. Bendegúz <bendicsekb@gmail.com>
2022-05-09 16:21:29 +0200

    Add visualization

--- visualize.py
+++ visualize.py
@@ -1,132 +1,205 @@
-#%%
 from cProfile import label
+from enum import Enum
+import os
 import dataset
 import discretization
 import main
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.collections as collections
 import json
 import pickle
 
-#%%
+def read_discretizations(file_base, type_of_file, disc_type, ds_type, eqf_bins, hist_maxbins, copy):
 
-ionosphere_dataset: dataset.DataSet = main.read_ionosphere("./data/Ionosphere/ionosphere.data")
-mammals_dataset: dataset.DataSet = main.read_mammals("./data/Mammals_dataset/mammals.arff")
-year_prediction_MSD_dataset : dataset.DataSet = main.read_YearPredictionMSD('./data/YearPredictionMSD/year_prediction.csv')
-
-
-
-
-
-
-# %%
-ds = mammals_dataset
-
-for descriptor_name in ds.descriptors.columns:
-    column = ds.descriptors[descriptor_name]
-    fig, ax = plt.subplots(3, 1)
-    ax[0].hist(column)
-    ax[0].set_title(descriptor_name)
-    fig.show()
+    if(disc_type == discretization.Discretizers.EQUAL_FREQUENCY):
+        boundary_filename = f'{file_base}{ds_type}_{disc_type}_{eqf_bins}bins_{type_of_file}'
+    elif(disc_type == discretization.Discretizers.HISTOGRAM):
+        boundary_filename = f'{file_base}{ds_type}_{disc_type}_{f"COPY_{eqf_bins}bins_" if copy else ""}max{hist_maxbins}bins_{type_of_file}'
+    else:
+        boundary_filename = 'No such discretizer'
+    with open(boundary_filename, 'rb') as f:
+        discretizations = json.load(f)
+    return discretizations
 
 
+def read_dump(file_base, type_of_file, disc_type, ds_type, eqf_bins, hist_maxbins, copy):
+    if(disc_type == discretization.Discretizers.EQUAL_FREQUENCY):
+        boundary_filename = f'{file_base}{ds_type}_{disc_type}_{eqf_bins}bins_{type_of_file}'
+    elif(disc_type == discretization.Discretizers.HISTOGRAM):
+        boundary_filename = f'{file_base}{ds_type}_{disc_type}_{f"COPY_{eqf_bins}bins_" if copy else ""}max{hist_maxbins}bins_{type_of_file}'
+    else:
+        boundary_filename = 'No such discretizer'
+    with open(boundary_filename, 'rb') as f:
+        dump = pickle.load(f)
+    return dump
 
 
-# %%
-with open('./save/json/Mammals_EQF_discretizations.json', 'r') as f:
-    eqf_discretizations = json.load(f)
-# %%
-eqf_discretizations[0]['max_temp_feb_utm']
-# %%
+class Grouping():
+    def __init__(self, discretizations, resultset, ds) -> None:
+        self.discretizations = discretizations
+        self.resultset = resultset
+        self.dataset = ds
 
-def find_discretizations(all_discretizations, descriptor_name):
+
+class TestType(Enum):
+    EQF = 1
+    HISTOGRAM = 2
+    COPY_HISTOGRAM = 3
+
+
+def keep_only_best(hist_resultset):
     ret = []
-    for discretizations in all_discretizations:
-        if descriptor_name in discretizations:
-            for disc in discretizations[descriptor_name]:
-                ret.append(disc[1])
+    for res_set in hist_resultset:
+        res_set[1].sort()
+        ret.append(res_set[1][-1])
+
     return ret
 
 
-# %%
-found = find_discretizations(eqf_discretizations, 'max_temp_feb_utm')
+def order_intervals(intervals):
+    ordered = {}
+    for k, interval in intervals.items():
+        ordered[k] = []
+        for cond in interval:
+            match cond.negated:
+                case False:
+                    ordered[k].insert(0, cond.value) # prepend
+                case True: 
+                    ordered[k].append(cond.value)
+    return ordered
 
 
+def extract_intervals(descr):
+    intervals = {}
+    for cond in descr:
+        if cond.attribute in intervals:
+            intervals[cond.attribute].append(cond)
+        else:
+            intervals[cond.attribute] = [cond]
 
-# %%
-len(found)
-# %%
-for f in found:
-    print(f)
-
-
-
-# %%
-mammals_dataset: dataset.DataSet = main.read_mammals("./data/Mammals_dataset/mammals.arff")
+    return order_intervals(intervals)
 
 
-mammals_resultset_eqf = []
-with open('./save/dump/Mammals_EQF_resultset.pickle', 'rb') as f:
-    results = pickle.load(f)
-for res in results:
-    mammals_resultset_eqf.append(res[2])
-
-histogram_quantizer = discretization.HistogramDiscretizer(10, mammals_dataset)
-
-# %%
-for i in range(len(mammals_resultset_eqf)):
-    subgroup_eqf = mammals_resultset_eqf[i]
-    discretization_boundaries = subgroup_eqf.discretization_boundaries
-    for attribute_name, boundaries in discretization_boundaries.items():
-        column = mammals_dataset.descriptors[attribute_name]
-        fig, ax = plt.subplots()
-        ax.hist(column)
-        level = 0
-        first = True
-        for _, boundary in boundaries:
-            bounds = [boundary[0][0]] + [b[1] for b in boundary]
-            for bound in bounds:
-                if first: 
-                    ax.axvline(bound, color='r', label='EQF', ymin=0.9+(level*0.05), ymax=0.999)
-                    first = False
-                else:
-                    ax.axvline(bound, color='r', ymin=0.9+(level*0.05), ymax=0.999)
-            level += 1
-
-        sorted = mammals_dataset.descriptors.loc[mammals_dataset.descriptors.index[mammals_dataset.descriptors[attribute_name].argsort()]]
-        splits: list[tuple] = histogram_quantizer.discretize(sorted[attribute_name])
-        boundary = [(sorted[attribute_name][sorted.index[split[0]]], sorted[attribute_name][sorted.index[split[1]]]) for split in splits]
+def plot_eq_hist(fig, ax, k, d_eqf, d_hist, column):
+    palette = ['r', 'm', 'y', 'b']
+    ax.hist(column, bins=25)
+    level = 0
+    first = True
+    for _, boundary in d_eqf:
         bounds = [boundary[0][0]] + [b[1] for b in boundary]
-        first = True
         for bound in bounds:
-                if first: 
-                    ax.axvline(bound, color='b', label='HIST', ymin=0.001, ymax=0.1)
-                    first = False
-                else:
-                    ax.axvline(bound, color='b', ymin=0.001, ymax=0.1)
-
-        ax.legend(loc=0)
-        ax.set_title(attribute_name)
-        fig.savefig(f'./save/images/{i}_{attribute_name}.png', facecolor='white')
+            if first: 
+                ax.axvline(bound, color=palette[level], label=f'EQF', ymin=0.8+(level*0.05), ymax=0.999)
+                first = False
+            else:
+                ax.axvline(bound, color=palette[level], ymin=0.8+(level*0.05), ymax=0.999)
+        level += 1
 
 
+    level = 0
+    first = True
+    for _, boundary in d_hist:
+        bounds = [boundary[0][0]] + [b[1] for b in boundary]
+        for bound in bounds:
+            if first: 
+                ax.axvline(bound, color='b', label='HIST', ymin=0.001, ymax=0.2)
+                first = False
+            else:
+                ax.axvline(bound, color='b', ymin=0.001, ymax=0.2)
+        level += 1
+
+    ax.legend()
+    ax.set_title(f'{k}')
+    return fig, ax
+
+def plot_subgroup(fig, ax, interval, is_eqf, facecolor):
+    bottom, top = ax.get_ylim()
+    if is_eqf:
+        o = (interval[0], top-10)
+    else:
+        o = (interval[0], bottom)
+    w = interval[1] - interval[0]
+    h = 10
+    rec = patches.Rectangle(o, w, h, facecolor = facecolor, alpha=0.6)
+    ax.add_patch(rec)
+    
+    return fig, ax
 
 
+def plot_results(fig, ax, eqf_res, hist_res, d_eqf, d_hist, ds):
 
-#%%
+    eqf_intervals = extract_intervals(eqf_res[2].description)
+    hist_intervals = extract_intervals(hist_res[2].description)
+
+    keys = d_eqf.keys()
+    for i, k in enumerate(keys):
+        fig, ax[i] = plot_eq_hist(fig, ax[i], k, d_eqf[k], d_hist[k], ds.descriptors[k])
+        fig, ax[i] = plot_subgroup(fig, ax[i], eqf_intervals[k], is_eqf=True, facecolor='g')
+        fig, ax[i] = plot_subgroup(fig, ax[i], hist_intervals[k], is_eqf=False, facecolor='m')
+        
+    fig.suptitle(f'EQF: {round(eqf_res[0], 5)} vs HIST: {round(hist_res[0], 5)}')
+    return fig, ax
 
 
+if __name__=='__main__':
+    for ds_type in [dataset.DataSets.IONOSPHERE, dataset.DataSets.MAMMALS]:
+        for eqf_bins in [5,10,15,20,25]:
+            hist_maxbins = 20
+
+            folder_name = f'save/images/copy/{ds_type}/eqf_{eqf_bins}_bins'
+            if(not os.path.exists(folder_name)):
+                os.makedirs(folder_name)
 
 
+            test_result = {}
+            for test_type in [TestType.EQF, TestType.COPY_HISTOGRAM]: # [TestType.EQF, TestType.HISTOGRAM, TestType.COPY_HISTOGRAM]:
+                match test_type:
+                    case TestType.EQF:
+                        disc_type = discretization.Discretizers.EQUAL_FREQUENCY
+                        copy = False
+                    case TestType.HISTOGRAM:
+                        disc_type = discretization.Discretizers.HISTOGRAM
+                        copy = False
+                    case TestType.COPY_HISTOGRAM:
+                        disc_type = discretization.Discretizers.HISTOGRAM
+                        copy = True
+                        hist_maxbins = 20  # fixed for COPY_HISTOGRAM
+
+                type_of_file = 'discretizations.json'
+                file_base = f'save/json/new/discretization/'
+                discr = read_discretizations(
+                    file_base, type_of_file, disc_type, ds_type, eqf_bins, hist_maxbins, copy)
+
+                type_of_file = 'resultset.pickle'
+                file_base = f'save/dump/new/resultset/'
+                resultset = read_dump(file_base, type_of_file, disc_type,
+                                    ds_type, eqf_bins, hist_maxbins, copy)
+
+                file_base = f'save/dump/new/dataset/'
+                type_of_file = 'dataset.pickle'
+                ds = read_dump(file_base, type_of_file, disc_type,
+                            ds_type, eqf_bins, hist_maxbins, copy)
+
+                group = Grouping(discr, resultset, ds)
+                test_result[test_type] = group
 
 
+            ds = test_result[TestType.EQF].dataset
+            eqf_resultset = test_result[TestType.EQF].resultset
+            hist_resultset = keep_only_best(test_result[TestType.COPY_HISTOGRAM].resultset)
 
-# %%
-attribute_name = 'max_temp_feb_utm'
-sorted = mammals_dataset.descriptors.loc[mammals_dataset.descriptors.index[mammals_dataset.descriptors[attribute_name].argsort()]]
-splits: list[tuple] = histogram_quantizer.discretize(sorted[attribute_name])
-boundary = [(sorted[attribute_name][sorted.index[split[0]]], sorted[attribute_name][sorted.index[split[1]]]) for split in splits]
-bounds = [boundary[0][0]] + [b[1] for b in boundary]
+            for eqf_res, hist_res in zip(eqf_resultset, hist_resultset):
+                d_eqf = eqf_res[2].discretization_boundaries
+                d_hist = hist_res[2].discretization_boundaries
+                plt.figure()
+                keys = d_eqf.keys()
+                fig, ax = plt.subplots(ncols=len(keys), figsize=(20,5))
+                fig, ax = plot_results(fig, ax, eqf_res, hist_res, d_eqf, d_hist, ds)
+                ratio = hist_res[0] / eqf_res[0]
+                file_name = f'{folder_name}/{str(round(ratio, 3)).replace(".", "_")}__{"_".join(keys)}'
+                plt.savefig(file_name)
+                plt.clf()
+                plt.cla()
+                plt.close('all')
 
-# %%
-bounds
-# %%
